ddaadf80083f7f031b1dde6acb545a03cef16554
gdejardi <32439531+gdejardi@users.noreply.github.com>
2018-10-19 10:41:31 +0200

    Update HelloWorld.java

--- TPPI/src/HelloWorld.java
+++ TPPI/src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World * HellaeDK	BAHEBXFHL	EVFP");
+		System.out.println("Hello World from jean le pd");
 	}
 }
